13f0a8ac5185b710a732951d3f3ae9e129fc4e51
99894 <99894@DESKTOP-K351R76>
2023-11-06 12:46:36 +0500

    EXPSERS: Show by Group Name

---
+++ Excel.Importer.MVC/ViewModels/ApplicantViewModel.cs
@@ -0,0 +1,17 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Collections;
+using System.Collections.Generic;
+using Excel.Importer.MVC.Models.Foundations.Applicants;
+
+namespace Excel.Importer.MVC.ViewModels
+{
+    public class ApplicantViewModel
+    {
+        public List<Applicant> Applicants { get; set; }
+        public Applicant Applicant { get; set; }
+    }
+}
